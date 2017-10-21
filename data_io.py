ad91a1cf5ba41d8a521e509d91d7f6f56d3d3d1c
varung97 <varung97@gmail.com>
2017-10-21 13:05:01 +0800

    Move data files to a separate folder

--- data_io.py
+++ data_io.py
@@ -1,5 +1,7 @@
+import os.path
 import numpy as np
 
+DATA_FOLDER = 'data'
 TRAIN_SAMPLES = 'X_train.npy'
 TRAIN_LABELS = 'y_train.npy'
 TEST_SAMPLES = 'X_test.npy'
@@ -7,18 +9,22 @@ TEST_LABELS = 'y_test.npy'
 
 
 def loadTrainData():
-    return np.load(TRAIN_SAMPLES), np.load(TRAIN_LABELS)
+    return (np.load(os.path.join(DATA_FOLDER, TRAIN_SAMPLES)),
+            np.load(os.path.join(DATA_FOLDER, TRAIN_LABELS)))
 
 
 def loadTestSamples():
-    return np.load(TEST_SAMPLES)
+    return np.load(os.path.join(DATA_FOLDER, TEST_SAMPLES))
 
 
 def writeTestLabels(labels):
-    np.save(TEST_LABELS, labels)
+    np.save(os.path.join(DATA_FOLDER, TEST_LABELS), labels)
 
 
 if __name__ == '__main__':
     trainSamples, trainLabels = loadTrainData()
+    print(trainSamples.shape, trainLabels.shape)
+
     writeTestLabels(trainLabels)
-    assert np.array_equal(np.load(TEST_LABELS), trainLabels)
+    assert np.array_equal(np.load(os.path.join(DATA_FOLDER, TEST_LABELS)),
+                          trainLabels)
