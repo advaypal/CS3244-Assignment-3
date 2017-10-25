ab2453e6f0e90c5093786b2724221a55c4f25ce5
Advay Pal <advaypal@hotmail.com>
2017-10-25 14:32:11 +0800

    Minor bugs

--- cnn.py
+++ cnn.py
@@ -9,6 +9,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 
+# Code for data augmentation
 # datagen = ImageDataGenerator(
 #         rotation_range=40,
 #         width_shift_range=0.2,
@@ -26,25 +27,25 @@ from keras.preprocessing.image import ImageDataGenerator
 
 NUM_CLASSES, IMAGE_ROWS, IMAGE_COLS = 7, 50, 37
 
-def augmentData(x_train, y_train, x_test):
+def processData(x_train, y_train, x_test):
     if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
+        x_train = x_train.reshape(x_train.shape[0], 1, IMAGE_ROWS, IMAGE_COLS)
+        x_test = x_test.reshape(x_test.shape[0], 1, IMAGE_ROWS, IMAGE_COLS)
+        input_shape = (1, IMAGE_ROWS, IMAGE_COLS)
     else:
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
+        x_train = x_train.reshape(x_train.shape[0], IMAGE_ROWS, IMAGE_COLS, 1)
+        x_test = x_test.reshape(x_test.shape[0], IMAGE_ROWS, IMAGE_COLS, 1)
+        input_shape = (IMAGE_ROWS, IMAGE_COLS, 1)
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     x_train /= 255
     x_test /= 255
     y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
-    return (x_train, y_train, x_test)
+    return (x_train, y_train, x_test, input_shape)
 
 
-def cnnTrain(x, y, batch_size=32, epochs=15):
+def cnnTrain(x, y, input_shape, batch_size=32, epochs=15):
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(5, 5),
                      activation='relu',
@@ -55,7 +56,7 @@ def cnnTrain(x, y, batch_size=32, epochs=15):
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer=keras.optimizers.Adadelta(),
@@ -66,14 +67,15 @@ def cnnTrain(x, y, batch_size=32, epochs=15):
               epochs=epochs,
               verbose=1,
               validation_split=0.2)
+    return model
 
 def cnnPredict(x, model):
     return model.predict(x)
 
 if __name__ == '__main__':
-    (inital_x_train, initial_y_train) = data_io.loadTrainData()
+    (initial_x_train, initial_y_train) = data_io.loadTrainData()
     initial_x_test =  data_io.loadTestSamples()
-    (x_train, y_train, x_test) = augmentData(initial_x_train, initial_y_train, initial_x_test)
-    model = cnnTrain(x_train, y_train)
+    (x_train, y_train, x_test, input_shape) = processData(initial_x_train, initial_y_train, initial_x_test)
+    model = cnnTrain(x_train, y_train, input_shape)
     # predictions = cnnPredict(x_test, model)
     # data_io.writeTestLabels(predictions)
